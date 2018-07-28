bb95b69023cf39071c45931532bffd1aede23397
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-28 11:51:56 +0300

    Add background color to navigation bar on scrolling

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems);
 });
+
+(function() {
+  document.addEventListener("scroll", function() {
+    var nav = document.querySelector(".navigation__nav");
+    window.scrollY > 10
+      ? (nav.style.backgroundColor = "rgba(0,0,0,.7)")
+      : (nav.style.backgroundColor = "");
+  });
+})();
